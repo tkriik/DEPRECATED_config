59423b003db5216729b64b09fd103843784158eb
Tanel Kriik <tanel.kriik@outlook.com>
2017-09-12 20:22:18 +0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# i3
+mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.config/i3status
+ln -sni $PWD/i3/i3.conf $HOME/.config/i3/config
+ln -sni $PWD/i3/i3status.conf $HOME/.config/i3status/config
+
+# vim
+ln -sni $PWD/vim/vimrc $HOME/.vimrc
+rm -rfi $HOME/.vim
+ln -sni $PWD/vim/vim $HOME/.vim
+
+# xresources
+ln -sni $PWD/xresources/Xresources $HOME/.Xresources
+rm -rfi $HOME/.Xresources.d
+ln -sni $PWD/xresources/Xresources.d $HOME/.Xresources.d
---
+++ vim/vim/syntax/json.vim
@@ -0,0 +1,4 @@
+setlocal autoindent
+setlocal expandtab
+setlocal shiftwidth=2
+setlocal softtabstop=2
