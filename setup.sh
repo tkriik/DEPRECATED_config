7b707f18afac5fab8163efd6584e31833ad4cea0
Tanel Kriik <tanel.kriik@outlook.com>
2017-09-15 18:08:11 +0300

    make setup.sh openbsd-compatible

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # fonts
-ln -sni $PWD/fonts/fonts.conf $HOME/.fonts.conf
+ln -snf $PWD/fonts/fonts.conf $HOME/.fonts.conf
 
 # i3
 mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/i3status
-ln -sni $PWD/i3/i3.conf $HOME/.config/i3/config
-ln -sni $PWD/i3/i3status.conf $HOME/.config/i3status/config
+ln -snf $PWD/i3/i3.conf $HOME/.config/i3/config
+ln -snf $PWD/i3/i3status.conf $HOME/.config/i3status/config
 
 # vim
-ln -sni $PWD/vim/vimrc $HOME/.vimrc
-rm -rfi $HOME/.vim
-ln -sni $PWD/vim/vim $HOME/.vim
+ln -snf $PWD/vim/vimrc $HOME/.vimrc
+rm -rf $HOME/.vim
+ln -snf $PWD/vim/vim $HOME/.vim
 
 # xresources
-ln -sni $PWD/xresources/Xresources $HOME/.Xresources
-rm -rfi $HOME/.Xresources.d
-ln -sni $PWD/xresources/Xresources.d $HOME/.Xresources.d
+ln -snf $PWD/xresources/Xresources $HOME/.Xresources
+rm -rf $HOME/.Xresources.d
+ln -snf $PWD/xresources/Xresources.d $HOME/.Xresources.d
